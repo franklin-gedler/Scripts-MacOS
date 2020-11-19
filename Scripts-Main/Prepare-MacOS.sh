fad2a5571e38b1c460f5c273c6b5c2aff6b21904
franklin-gedler <tec.franklin.gedler@gmail.com>
2020-11-19 15:26:40 -0300

    Update 19-11-2020

--- Scripts-Main/Prepare-MacOS.sh
+++ Scripts-Main/Prepare-MacOS.sh
@@ -255,7 +255,7 @@ InstallGoogleChrome(){
 	echo "====================================================="
 	echo "              Descargando Google Chrome"
 	echo "====================================================="
-	curl -LO https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+	curl -LO# https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 	echo ""
 
 	echo "====================================================="
@@ -268,6 +268,46 @@ InstallGoogleChrome(){
 	echo "Listo . . ."
 	echo ""
 
+}
+
+InstallTeamViewerQS(){
+
+	echo "====================================================="
+	echo "              Instalando Team Viewer QS"
+	echo "====================================================="
+
+	# Variables que puedes tocar
+	name="TeamViewerQSMacOS.zip"
+	GITHUB_API_TOKEN="22c32578f7471802dd12913947471829fc892717"
+	owner="franklin-gedler"
+	repo="Only-Download"
+	tag="3"
+
+	# Variables que no se tocan
+	GH_API="https://api.github.com"
+	GH_REPO="$GH_API/repos/$owner/$repo"
+	GH_TAGS="$GH_REPO/releases/tags/$tag"
+	AUTH="Authorization: token $GITHUB_API_TOKEN"
+	CURL_ARGS="-LJO#"
+
+	response=$(curl -sH "$AUTH" $GH_TAGS)
+	eval $(echo "$response" | grep -C3 "name.:.\+$name" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+	#[ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
+	GH_ASSET="$GH_REPO/releases/assets/$id"
+	#echo "$GH_ASSET"
+
+	# Download asset file.
+	#echo "Downloading asset..." >&2
+	curl $CURL_ARGS -H 'Accept: application/octet-stream' "$GH_ASSET?access_token=$GITHUB_API_TOKEN"
+	#echo "$0 done." >&2
+
+	unzip TeamViewerQSMacOS.zip 1>/dev/null
+
+	cp -r TeamViewerQS.app /Applications/
+
+	echo " ============== "
+	echo "   Listo . . .  "
+	echo " ============== "
 }
 
 ping -c1 google.com &>/dev/null
@@ -309,6 +349,7 @@ else
 	InstallGoogleChrome
 	FileVault
 	Glpi
+	InstallTeamViewerQS
 	last7serial=$(echo $serial | tail -c 8 | tr -d '[[:space:]]')
 	newpass="*+54#$last7serial*"
 	dscl . -passwd /Users/admindesp $currentpass $newpass
