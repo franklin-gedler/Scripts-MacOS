c3e36856b697393e3902bfa3484ead9c3a4011a8
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-10-04 11:10:12 -0300

    Update 4-10-2021

--- Scripts-Main/Prepare-MacOS.sh
+++ Scripts-Main/Prepare-MacOS.sh
@@ -476,37 +476,6 @@ InstallTeamViewerHost(){
 	installer -pkg TeamViewerHost-idc6gh9rc7.pkg -target /
 	hdiutil detach /Volumes/TeamViewerHost/ 1>/dev/null
 
-	<<-!
-	# Variables que puedes tocar
-	name="TeamViewerQSMacOS.zip"
-	GITHUB_API_TOKEN="22c32578f7471802dd12913947471829fc892717"
-	owner="franklin-gedler"
-	repo="Only-Download"
-	tag="3"
-
-	# Variables que no se tocan
-	GH_API="https://api.github.com"
-	GH_REPO="$GH_API/repos/$owner/$repo"
-	GH_TAGS="$GH_REPO/releases/tags/$tag"
-	AUTH="Authorization: token $GITHUB_API_TOKEN"
-	CURL_ARGS="-LJO#"
-
-	response=$(curl -sH "$AUTH" $GH_TAGS)
-	eval $(echo "$response" | grep -C3 "name.:.\+$name" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-	#[ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
-	GH_ASSET="$GH_REPO/releases/assets/$id"
-	#echo "$GH_ASSET"
-
-	# Download asset file.
-	#echo "Downloading asset..." >&2
-	#curl $CURL_ARGS -H 'Accept: application/octet-stream' "$GH_ASSET?access_token=$GITHUB_API_TOKEN"
-	curl $CURL_ARGS -H "Authorization: token $GITHUB_API_TOKEN" -H "Accept: application/octet-stream" "$GH_ASSET"
-	#echo "$0 done." >&2
-
-	unzip TeamViewerQSMacOS.zip 1>/dev/null
-
-	cp -r TeamViewerQS.app /Applications/
-	!
 	echo ""
 	echo " *************** "
 	echo "   Listo . . .   "
